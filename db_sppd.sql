b6bc3387350e6be8f1f5ca985f6b07ea118cb5c6
Arissatur Rohman <54654189+arissaturrohman@users.noreply.github.com>
2020-12-30 13:39:49 +0700

    update database

--- db_sppd.sql
+++ db_sppd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2020 at 09:33 AM
+-- Generation Time: Dec 30, 2020 at 07:37 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -100,6 +100,36 @@ INSERT INTO `tb_pegawai` (`id_pegawai`, `nip`, `nama_pegawai`, `id_jabatan`, `id
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_sppd`
+--
+
+CREATE TABLE `tb_sppd` (
+  `id_sppd` int(11) NOT NULL,
+  `id_pegawai` int(11) NOT NULL,
+  `surat` varchar(100) NOT NULL,
+  `nomor` varchar(50) NOT NULL,
+  `tgl_surat` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `perihal` varchar(100) NOT NULL,
+  `kegiatan` varchar(100) NOT NULL,
+  `tujuan` varchar(100) NOT NULL,
+  `lama` int(11) NOT NULL,
+  `tgl_berangkat` date NOT NULL,
+  `tgl_pulang` date NOT NULL,
+  `pengikut1` int(11) NOT NULL,
+  `pengikut2` int(11) NOT NULL,
+  `pengikut3` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_sppd`
+--
+
+INSERT INTO `tb_sppd` (`id_sppd`, `id_pegawai`, `surat`, `nomor`, `tgl_surat`, `perihal`, `kegiatan`, `tujuan`, `lama`, `tgl_berangkat`, `tgl_pulang`, `pengikut1`, `pengikut2`, `pengikut3`) VALUES
+(1, 1, 'Bappeda Kab. Demak', '005/205/I/2021', '2020-12-28 17:00:00', 'Undangan', 'Rapat Teknis Penyusunan LPPD', 'Hotel Lor In Solo', 1, '2020-12-29', '2020-12-30', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_tingkat`
 --
 
@@ -116,6 +146,28 @@ INSERT INTO `tb_tingkat` (`id_tingkat`, `tingkat`) VALUES
 (1, 'Tingkat C'),
 (2, 'Tingkat D'),
 (3, 'Tingkat E');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id_user` int(11) NOT NULL,
+  `nip` varchar(30) NOT NULL,
+  `id_pegawai` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` enum('admin','pegawai') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_user`
+--
+
+INSERT INTO `tb_user` (`id_user`, `nip`, `id_pegawai`, `password`, `level`) VALUES
+(1, 'admin', 0, '$2y$10$tVrX0FTJb3kVB4fzmYTyeeAfuZgt9F9L5fJAhLQyyJUzpQ.C9a9Sy', 'admin'),
+(2, '197204011992031005', 1, '$2y$10$k4Bs31mMImYHOHYlQG2MTe4lHcra29MhnOkZjP27ohWxwSGbOr.vK', 'pegawai');
 
 --
 -- Indexes for dumped tables
@@ -140,10 +192,22 @@ ALTER TABLE `tb_pegawai`
   ADD PRIMARY KEY (`id_pegawai`);
 
 --
+-- Indexes for table `tb_sppd`
+--
+ALTER TABLE `tb_sppd`
+  ADD PRIMARY KEY (`id_sppd`);
+
+--
 -- Indexes for table `tb_tingkat`
 --
 ALTER TABLE `tb_tingkat`
   ADD PRIMARY KEY (`id_tingkat`);
+
+--
+-- Indexes for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -168,10 +232,22 @@ ALTER TABLE `tb_pegawai`
   MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `tb_sppd`
+--
+ALTER TABLE `tb_sppd`
+  MODIFY `id_sppd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `tb_tingkat`
 --
 ALTER TABLE `tb_tingkat`
   MODIFY `id_tingkat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
